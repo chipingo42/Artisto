c51bdb51dadcebb963eeb3f0c66d730f712211ff
victor <82973437+chipingo42@users.noreply.github.com>
2023-01-01 11:56:49 +0100

    new check

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -6,14 +6,14 @@ import avater from '../Assets/Group7-down.png'
 const Cards = () => {
   return (
     <>
-      <div className=' text-white max-w-[1200px] mx-auto'>
+      <div className='text-white max-w-[1200px] mx-auto'>
         <div className=' flex flex-wrap justify-between space-y-[40px]  items-center mt-[71px]'>
           <Link to="Cardspages">
             <div className=' w-[366px] h-[471px] bg-[#1A1405] rounded-tl-[150px] rounded-br-[200px]'>
               <img src={face_image} alt="/" />
               <div className='px-[10px] '>
                 <h1 className='font-Sora font-[800] text-[24px] pt-10'>Plastic Hears Sculpture</h1>
-                <p className='font-Sora font-[400] text-[20px] pt-3 '>11- 12 November, 2021</p>
+                <p className='font-Sora font-[400] text-[20px] pt-3'>11- 12 November, 2021</p>
                 <p className='font-Sora font-[400] text-[20px]'>Galeri Salihara, Indonesia</p>
               </div>
             </div>
@@ -39,7 +39,7 @@ const Cards = () => {
             </div>
           </Link>
           <Link to="Cardspages">
-            <div className=' w-[366px] h-[471px] bg-[#1A1405] rounded-tl-[150px] rounded-br-[200px]'>
+            <div className='w-[366px] h-[471px] bg-[#1A1405] rounded-tl-[150px] rounded-br-[200px]'>
               <img src={face_image} alt="/" />
               <div className='px-[10px] '>
                 <h1 className='font-Sora font-[800] text-[24px] pt-10'>Plastic Hears Sculpture</h1>
@@ -49,7 +49,7 @@ const Cards = () => {
             </div>
           </Link>
           <Link to="Cardspages">
-            <div className=' w-[366px] h-[471px] bg-[#1A1405] rounded-tl-[150px] rounded-br-[200px]'>
+            <div className='w-[366px] h-[471px] bg-[#1A1405] rounded-tl-[150px] rounded-br-[200px]'>
               <img src={face_image} alt="/" />
               <div className='px-[10px] '>
                 <h1 className='font-Sora font-[800] text-[24px] pt-10'>Plastic Hears Sculpture</h1>
@@ -59,7 +59,7 @@ const Cards = () => {
             </div>
           </Link>
           <Link to="Cardspages">
-            <div className=' w-[366px] h-[471px] bg-[#1A1405] rounded-tl-[150px] rounded-br-[200px]'>
+            <div className='w-[366px] h-[471px] bg-[#1A1405] rounded-tl-[150px] rounded-br-[200px]'>
               <img src={face_image} alt="/" />
               <div className='px-[10px] '>
                 <h1 className='font-Sora font-[800] text-[24px] pt-10'>Plastic Hears Sculpture</h1>
